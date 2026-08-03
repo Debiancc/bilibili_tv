55dd1c0b000dc18435445f495a44544295049dd9
Debiancc <debiancc@users.noreply.github.com>
2026-08-03 21:38:37 +0800

    fix: address CodeRabbit review feedback on stats monitoring

--- bilibili_tv/Features/Player/PlayerStatsViewModel.swift
+++ bilibili_tv/Features/Player/PlayerStatsViewModel.swift
@@ -100,7 +100,11 @@ class PlayerStatsViewModel {
             let bitrate = lastEvent.observedBitrate
             if bitrate > 0 {
                 connectionSpeed = String(format: "%.0f Kbps", bitrate / 1024)
+            } else {
+                connectionSpeed = "0 Kbps"
             }
+        } else {
+            connectionSpeed = "0 Kbps"
         }
         }
         
